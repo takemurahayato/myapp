f28adc2693ece66c88d994adfc5011d314b97db4
takemura hayato <you@example.com>
2021-03-26 21:03:58 +0900

    'lunchmap'

---
+++ lunchmap/views.py
@@ -0,0 +1,21 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import Category, Shop
+
+class IndexView(generic.ListView):
+    model = Shop
+
+class DetailView(generic.DetailView):
+    model = Shop
+
+class CreateView(generic.edit.CreateView):
+    model = Shop
+    fields = '__all__'
+
+class UpdateView(generic.edit.UpdateView):
+    model = Shop
+    fields = '__all__'
+
+class DeleteView(generic.edit.DeleteView):
+    model = Shop
+    success_url = reverse_lazy('lunchmap:index')
